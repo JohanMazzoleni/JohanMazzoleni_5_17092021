0f955878c30caf3696a67a7a8b6aaccf4ae35673
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-09-25 15:45:54 +0200

    fix(front-end): rename variable && change onclick

--- js/product.js
+++ js/product.js
@@ -1,4 +1,4 @@
-const end_point = "http://localhost:3000/";
+const endpoint = "http://localhost:3000/";
 
 /**
  * Permet de transformer les Integers en format de prix français.
@@ -16,9 +16,9 @@ var formatter = new Intl.NumberFormat('fr-Fr', {
  * @return { Promise }
 */
 
-function ProductDetails(id) {
+function productDetail(id) {
 	return new Promise(function (resolve, reject) {
-		fetch(end_point + "api/cameras/" + id).then(async function (data) {
+		fetch(endpoint + "api/cameras/" + id).then(async function (data) {
 			resolve(await data.json());
 		})
 			.catch(function (err) {
@@ -32,7 +32,7 @@ function ProductDetails(id) {
  * @param  { Object } data
 */
 
-function GenerateSelect(data) {
+function generateSelect(data) {
 	var code = ``
 	for (let index = 0; index < data.lenses.length; index++) {
 		code = code + `<option value="${index}">${data.lenses[index]}</option>`
@@ -45,7 +45,7 @@ function GenerateSelect(data) {
  * @param  { Object } data
 */
 
-function AddToBasket(data) {
+function addToBasket(data) {
 	var basket_storage = localStorage.getItem("basket");
 	if (basket_storage) {
 		var basket_data = JSON.parse(basket_storage);
@@ -57,32 +57,32 @@ function AddToBasket(data) {
 	}
 }
 
-window.onload = async function () {
-	const product_name = document.getElementById("product-name");
-	const product_image = document.getElementById("product-image");
+window.addEventListener("DOMContentLoaded", async () => {
+	const id = new URL(location.href).searchParams.get("id");
+
+	const name = document.getElementById("product-name");
+	const image = document.getElementById("product-image");
 	const description = document.getElementById("description");
 	const price = document.getElementById("price");
 	const lenses = document.getElementById("lenses");
 
-	const id = new URL(location.href).searchParams.get("id");
+	const product = await productDetail(id);
 
-	const product_details = await ProductDetails(id);
+	name.innerText = product.name;
+	image.setAttribute("src", product.imageUrl);
+	description.innerText = product.description;
+	price.innerText = formatter.format(product.price);
+	lenses.innerHTML = generateSelect(product);
 
-	product_name.innerText = product_details.name;
-	product_image.setAttribute("src", product_details.imageUrl);
-	description.innerText = product_details.description;
-	price.innerText = formatter.format(product_details.price);
-	lenses.innerHTML = GenerateSelect(product_details);
-
-	document.getElementById("add-to-basket").onclick = function () {
-		AddToBasket({
-			name: product_details.name,
+	document.getElementById("add-to-basket").addEventListener("click", function () {
+		addToBasket({
+			name: product.name,
 			accesories: lenses.options[lenses.selectedIndex].text,
-			_id: product_details._id,
-			price: product_details.price,
+			_id: product._id,
+			price: product.price,
 		});
 
 		location.href = "./panier.html";
-	}
-};
+	})
 
+});
