569de9273eb04caf537c1c797657a29f9e6103de
JohanMazzoleni <85082764+JohanMazzoleni@users.noreply.github.com>
2021-10-01 17:30:52 +0200

    fix(front-end): remove basket at end

--- js/order.js
+++ js/order.js
@@ -1,4 +1,6 @@
 window.addEventListener("DOMContentLoaded", async () => {
     const id = new URL(location.href).searchParams.get("id");
     document.getElementById("order-id").innerText = id;
+
+    localStorage.removeItem("basket");
 });
